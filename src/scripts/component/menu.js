5047f595fe04521f2fa825a36cf35bb073f32301
ykosinets <ykosinets@gmail.com>
2020-06-10 22:36:28 +0300

    headers update (split)

--- src/scripts/component/menu.js
+++ src/scripts/component/menu.js
@@ -1,5 +1,6 @@
 let $indexPageButtons = $('.toggle-button');
 let $indexPageToggleButton = $('.toggle-button');
+let $expand = $('#course-type-container');
 let $indexPageMenu = $('.navigation');
 let $body = $('body');
 let $window = $(window);
@@ -10,15 +11,31 @@ if ($indexPageButtons.length) {
     $indexPageToggleButton.toggleClass('active');
     $indexPageMenu.toggleClass('active');
     $body.toggleClass('menu-open');
-    if($body.hasClass('menu-open')){
+    if ($body.hasClass('menu-open')) {
       $body.css('top', '-' + document.documentElement.style.getPropertyValue('--scroll-y'));
-    }else {
-      window.scrollTo(0, parseInt($body.css('top') || '0') * -1);
-      $body.css('top', 'auto');
+    } else {
+      if ($expand.hasClass('active')) {
+        $expand.removeClass('active');
+      } else {
+        window.scrollTo(0, parseInt($body.css('top') || '0') * -1);
+        $body.css('top', 'auto');
+      }
     }
   });
+
   $window
     .on('scroll', function () {
       document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
     })
 }
+
+
+document.addEventListener('click', (e) => {
+  let isClickInside = $expand[0].contains(e.target);
+  console.log(isClickInside);
+
+  if (!isClickInside) {
+    $expand.removeClass('active');
+  }
+});
+
